163a1fc620b33712de1f8dbc05bcc5c9dccfeac3
Seth <surban@prototest.com>
2013-07-17 11:38:11 -0600

    Initial Checkin

---
+++ PageObjects.ProtoTest/ProtoTest_Home.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Golem.Framework;
+using OpenQA.Selenium;
+
+namespace PageObjects.ProtoTest
+{
+    public class ProtoTest_Home
+    {
+        Element ProtoTest_Logo = new Element("ProtoTest_Logo", By.Id("logo"));
+
+    }
+}
---
+++ Tests.ProtoTest/HomePage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Golem.Framework;
+using PageObjects.ProtoTest;
+using MbUnit.Framework;
+
+namespace Tests.ProtoTest
+{
+    public class HomePage
+    {
+    }
+}
